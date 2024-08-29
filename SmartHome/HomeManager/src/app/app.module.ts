c5b25b9db3778dacf1f586a6bd533413dbdbcec9
andyrrr <43052958+andyrrr@users.noreply.github.com>
2024-08-28 22:02:39 -0600

    login

--- SmartHome/HomeManager/src/app/app.module.ts
+++ SmartHome/HomeManager/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CasaComponent } from './casa/casa.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CasaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
